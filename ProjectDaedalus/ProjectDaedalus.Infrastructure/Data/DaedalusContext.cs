baf93d61d2e3379303f9ea613e8aab78a45fcc24
MarkShort <mlshorter17@gmail.com>
2025-09-03 10:23:22 -0400

    chore(git): Creating and updating git ignore

--- ProjectDaedalus/ProjectDaedalus.Infrastructure/Data/DaedalusContext.cs
+++ ProjectDaedalus/ProjectDaedalus.Infrastructure/Data/DaedalusContext.cs
@@ -28,8 +28,6 @@ public partial class DaedalusContext : DbContext
     public virtual DbSet<UserPlant> UserPlants { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=daedalus_db;user=shorterm;password=daedalus", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.43-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
