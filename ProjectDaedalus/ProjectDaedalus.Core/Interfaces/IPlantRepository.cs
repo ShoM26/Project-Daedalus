fa5ec478bb8bec3b0ea25e829ba5f81c1cb50d1e
MarkShort <mlshorter17@gmail.com>
2025-09-20 18:29:40 -0400

    feat(RegistrationScript): Wrote the repository methods for the bulkplantregistration

--- ProjectDaedalus/ProjectDaedalus.Core/Interfaces/IPlantRepository.cs
+++ ProjectDaedalus/ProjectDaedalus.Core/Interfaces/IPlantRepository.cs
@@ -6,6 +6,20 @@ namespace ProjectDaedalus.Core.Interfaces
     {
         Task<Plant?> GetByScientificNameAsync(string scientificName);
         Task<bool> ScientificNameExistAsync(string scientificName);
+        
+        Task<BulkInsertResult<Plant>> BulkInsertAsync(IEnumerable<Plant> plants);
     }
+}
+public class BulkInsertResult<T>
+{
+    public int TotalPlants { get; set; }
+    public int SuccessfulRegistrations { get; set; }
+    public int FailedRegistrations { get; set; }
+    
+    public List<T> CreatedItems { get; set; } = new List<T>();
+    public string ErrorMessage { get; set; }
+    
+    public bool IsSuccess => FailedRegistrations == 0 && SuccessfulRegistrations > 0;
+    public bool HasErrors => ErrorMessage.Length == 0 || FailedRegistrations > 0;
 }
 
