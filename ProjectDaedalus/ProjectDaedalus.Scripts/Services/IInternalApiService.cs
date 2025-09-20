cf076935874dbe6cceb3763d1bf0650eb819f262
MarkShort <mlshorter17@gmail.com>
2025-09-20 15:56:09 -0400

    Decided on an inline interface definition since this should be a minute class

--- ProjectDaedalus/ProjectDaedalus.Scripts/Services/IInternalApiService.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjectDaeadalus.Scripts.Services;
-
-public interface IInternalApiService
-{
-    Task<T> PostManyAsync<T>(string endpoint, IEnumerable<object> data);
-    Task<T> GetAsync<T>(string endpoint);
-    Task<bool> DeleteAsync(string endpoint);
-}
