bf8943b080d5aac21f75f67fc9d667260c8e0f3d
MarkShort <mlshorter17@gmail.com>
2025-10-07 15:16:05 -0400

    test(): To see if ai assistant went to sleep or not

---
+++ ProjectDaedalus/ProjectDaedalus.Scripts/Scripts/NewScript.cs
@@ -0,0 +1,15 @@
+using ProjectDaedalus.Core.Entities;
+
+namespace ProjectDaedalus.Scripts.Scripts
+{
+    public class NewScript
+    {
+        public async Task<Device> ExecuteAsync()
+        {
+            return new Device()
+            {
+                
+            };
+        }
+    }
+}
