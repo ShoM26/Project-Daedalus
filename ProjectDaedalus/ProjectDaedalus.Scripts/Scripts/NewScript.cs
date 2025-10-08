4406ffaf3683f8c3ee9556ac82abe28653e10eb1
MarkShort <mlshorter17@gmail.com>
2025-10-08 14:57:35 -0400

    test(): I think I need to change code not comments

--- ProjectDaedalus/ProjectDaedalus.Scripts/Scripts/NewScript.cs
+++ ProjectDaedalus/ProjectDaedalus.Scripts/Scripts/NewScript.cs
@@ -12,6 +12,8 @@ namespace ProjectDaedalus.Scripts.Scripts
             var random = new Random();
             var x = random.NextInt64();
             Console.WriteLine(x > 5 ? "x is greater than 5" : "x is less than 5");
+            x = x*x + random.NextInt64();
+            Console.WriteLine(x);
             return new Device()
             {
                 //GEMINI RIGHTS WAS AN OVERHYPED STEVE LACY ALBUM
@@ -23,6 +25,7 @@ namespace ProjectDaedalus.Scripts.Scripts
             };
             //Hello AI this is a test to make sure you can comment on future prs
             //Do you answer questions if I ask them in comments?
+            
         }
     }
 }
