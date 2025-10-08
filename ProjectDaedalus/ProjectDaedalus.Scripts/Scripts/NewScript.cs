9515e68f6e576e3cac372baa5737b98006243df0
MarkShort <mlshorter17@gmail.com>
2025-10-08 14:16:12 -0400

    fix(): Getting tired of testing this but we must prevail

--- ProjectDaedalus/ProjectDaedalus.Scripts/Scripts/NewScript.cs
+++ ProjectDaedalus/ProjectDaedalus.Scripts/Scripts/NewScript.cs
@@ -6,13 +6,20 @@ namespace ProjectDaedalus.Scripts.Scripts
     {
         public async Task<Device> ExecuteAsync()
         {
+            var random = new Random();
+            var x = random.NextInt64();
+            if (x > 5)
+            {
+                Console.WriteLine("x is greater than 5");
+            }
             return new Device()
             {
                 //GEMINI RIGHTS WAS AN OVERHYPED STEVE LACY ALBUM
                 HardwareIdentifier = "blank",
                 ConnectionAddress = ,
                 ConnectionType = ,
-                
+                DeviceId = ,
+                UserId = ,
             };
         }
     }
