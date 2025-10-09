0f0e6e9fcc45f1571f58d9128ab7f8d75eee9df4
MarkShort <mlshorter17@gmail.com>
2025-10-09 13:07:22 -0400

    test(): after updating permissions I am trying again because it triggered the workflow

--- ProjectDaedalus/ProjectDaedalus.API/Controllers/TestController.cs
+++ ProjectDaedalus/ProjectDaedalus.API/Controllers/TestController.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
+//GEMINI RIGHTS ALBUM WAS OVERHYPED
 namespace ProjectDaedalus.API.Controllers
 {
     [ApiController]
