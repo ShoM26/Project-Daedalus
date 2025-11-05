bb41986ea1e6745798d9107848a936a6eefcd091
MarkShort <mlshorter17@gmail.com>
2025-11-04 19:26:12 -0500

    feat(): Creating the required two entities for this notification feature

---
+++ ProjectDaedalus/ProjectDaedalus.Core/Entities/Notification.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectDaedalus.Core.Entities
+{
+    [Table("notification")]
+    public class Notification
+    {
+        [Key]
+        [Column("notification_id")]
+        public int NotificationId { get; set; }
+        
+        [Column("userplant_id")]
+        [Required]
+        public int UserPlantId { get; set; }
+        
+        [Column("message")]
+        [MaxLength(255)]
+        [Required]
+        public string Message { get; set; }
+        [Column("notification_type")]
+        public NotificationType NotificationType { get; set; }
+
+        [Column("is_read")] public bool IsRead { get; set; } = false;
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+        
+        //Navigation properties
+        public required UserPlant UserPlant { get; set; }
+    }
+}
+
+public enum NotificationType
+{
+    LowMoisture = 1,
+    HighMoisture = 2,
+    SystemAlert = 3
+}
