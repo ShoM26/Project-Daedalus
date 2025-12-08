86ae3445190fd2412009c1558770c75ae2e6ec04
MarkShort <mlshorter17@gmail.com>
2025-12-08 10:33:07 -0500

    fix(AutoDevice): no longer have to worry about accounting for post,  created its own dto

--- ProjectDaedalus/ProjectDaedalus.API/Dtos/Device/DeviceDto.cs
+++ ProjectDaedalus/ProjectDaedalus.API/Dtos/Device/DeviceDto.cs
@@ -2,7 +2,7 @@ namespace ProjectDaedalus.API.Dtos.Device
 {
     public class DeviceDto
     {
-        public int? DeviceId { get; set; } // Nullable for POST requests
+        public int DeviceId { get; set; } // Nullable for POST requests
         public string HardwareIdentifier { get; set; } = string.Empty;
         public string ConnectionType { get; set; } = string.Empty;
         public string ConnectionAddress { get; set; } = string.Empty;
