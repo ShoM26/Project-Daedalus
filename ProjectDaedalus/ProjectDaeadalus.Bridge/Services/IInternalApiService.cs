839765b1f3ca302905434600cfcbb72aeae8739f
MarkShort <mlshorter17@gmail.com>
2025-12-08 10:27:22 -0500

    feat(AutoDevice): Adding Get and Put calls to the InternalApiService.cs

--- ProjectDaedalus/ProjectDaeadalus.Bridge/Services/IInternalApiService.cs
+++ ProjectDaedalus/ProjectDaeadalus.Bridge/Services/IInternalApiService.cs
@@ -3,4 +3,6 @@ namespace ProjectDaeadalus.Bridge.Services;
 public interface IInternalApiService
 {
     Task<T> PostAsync<T>(string endpoint, object data);
+    Task<T> GetAsync<T>(string endpoint);
+    Task<T> PutAsync<T>(string endpoint, object data);
 }
--- ProjectDaedalus/ProjectDaeadalus.Bridge/Services/InternalApiService.cs
+++ ProjectDaedalus/ProjectDaeadalus.Bridge/Services/InternalApiService.cs
@@ -12,21 +12,47 @@ namespace ProjectDaeadalus.Bridge.Services
         {
             _httpClient = httpClient;
             _baseUrl = config["ApiSettings:BaseUrl"];
-        
+
             var apiKey = config["ApiSettings:ApiKey"];
             _httpClient.DefaultRequestHeaders.Add("X-API-Key", apiKey);
         }
+
+        private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // This fixes the CamelCase vs PascalCase issue
+        };
 
         public async Task<T> PostAsync<T>(string endpoint, object data)
         {
             var json = JsonSerializer.Serialize(data);
             var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-        
+
             var response = await _httpClient.PostAsync($"{_baseUrl}/{endpoint}", content);
             response.EnsureSuccessStatusCode();
-        
+
             var responseJson = await response.Content.ReadAsStringAsync();
-            return JsonSerializer.Deserialize<T>(responseJson);
+            return JsonSerializer.Deserialize<T>(responseJson, _jsonOptions);
+        }
+        
+        public async Task<T> PutAsync<T>(string endpoint, object data)
+        {
+            var json = JsonSerializer.Serialize(data);
+            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"{_baseUrl}/{endpoint}", content);
+            response.EnsureSuccessStatusCode();
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseJson, _jsonOptions);
+        }
+
+        public async Task<T> GetAsync<T>(string endpoint)
+        {
+            var response = await _httpClient.GetAsync($"{_baseUrl}/{endpoint}");
+            response.EnsureSuccessStatusCode();
+            var responseJson = await response.Content.ReadAsStringAsync();
+            
+            return JsonSerializer.Deserialize<T>(responseJson, _jsonOptions);
         }
     }
 }
