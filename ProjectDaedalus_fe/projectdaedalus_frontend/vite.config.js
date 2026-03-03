da81c3f1ab994e7ad0496e9c3e8b1d8e2987f4a2
MarkShort <mlshorter17@gmail.com>
2026-03-03 15:44:17 -0500

    fix(): Adding the base to the vite config file

--- ProjectDaedalus_fe/projectdaedalus_frontend/vite.config.js
+++ ProjectDaedalus_fe/projectdaedalus_frontend/vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@features': path.resolve(__dirname, './src/features'),
